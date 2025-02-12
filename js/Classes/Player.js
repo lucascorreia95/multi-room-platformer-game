71173a28db371343bade6ee365900d5bcff6c036
Lucas Correia <lucascorreia-.-@live.com>
2025-02-12 15:38:44 -0300

    feat: create the hitbox

--- js/Classes/Player.js
+++ js/Classes/Player.js
@@ -21,18 +21,23 @@ class Player extends Sprite {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
       const collisionBlock = this.collisionBlocks[i];
 
-      if (this.position.x <= collisionBlock.position.x + collisionBlock.width && 
-        this.position.x + this.width >= collisionBlock.position.x &&
-        this.position.y + this.height >= collisionBlock.position.y &&
-        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      if (
+        this.hitBox.position.x <= collisionBlock.position.x + collisionBlock.width && 
+        this.hitBox.position.x + this.hitBox.width >= collisionBlock.position.x &&
+        this.hitBox.position.y + this.hitBox.height >= collisionBlock.position.y &&
+        this.hitBox.position.y <= collisionBlock.position.y + collisionBlock.height
       ) {
         if (this.velocity.x < 0) {
-          this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01;
+          const offset = this.hitBox.position.x - this.position.x;
+
+          this.position.x = collisionBlock.position.x + collisionBlock.width - offset + 0.01;
           break;
         };
 
         if (this.velocity.x > 0) {
-          this.position.x = collisionBlock.position.x - this.width - 0.01;
+          const offset = this.hitBox.position.x - this.position.x + this.hitBox.width;
+
+          this.position.x = collisionBlock.position.x - offset - 0.01;
           break;
         };
       };
@@ -48,20 +53,25 @@ class Player extends Sprite {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
       const collisionBlock = this.collisionBlocks[i];
 
-      if (this.position.x <= collisionBlock.position.x + collisionBlock.width && 
-        this.position.x + this.width >= collisionBlock.position.x &&
-        this.position.y + this.height >= collisionBlock.position.y &&
-        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      if (
+        this.hitBox.position.x <= collisionBlock.position.x + collisionBlock.width && 
+        this.hitBox.position.x + this.hitBox.width >= collisionBlock.position.x &&
+        this.hitBox.position.y + this.hitBox.height >= collisionBlock.position.y &&
+        this.hitBox.position.y <= collisionBlock.position.y + collisionBlock.height
       ) {
         if (this.velocity.y < 0) {
+          const offset = this.hitBox.position.y - this.position.y;
+
           this.velocity.y = 0;
-          this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01;
+          this.position.y = collisionBlock.position.y + collisionBlock.height - offset + 0.01;
           break;
         };
 
         if (this.velocity.y > 0) {
+          const offset = this.hitBox.position.y - this.position.y + this.hitBox.height;
+
           this.velocity.y = 0;
-          this.position.y = collisionBlock.position.y - this.height - 0.01;
+          this.position.y = collisionBlock.position.y - offset - 0.01;
           break;
         };
       };
@@ -71,8 +81,21 @@ class Player extends Sprite {
   update() {
     this.position.x += this.velocity.x;
 
+    this.updateHitBox();
     this.checkHorizontalCollisions();
     this.applyGravity();
+    this.updateHitBox();
     this.checkVerticalCollisions();
+  };
+
+  updateHitBox() {
+    this.hitBox = {
+      position: {
+        x: this.position.x + 58,
+        y: this.position.y + 34,
+      },
+      width: 50,
+      height: 53,
+    };
   };
 };
